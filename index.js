3138f9e0078245b23d31acd6fd69404ae9fcb16b
TONY ADAMS <truvisory@TONYs-MacBook-Pro.local>
2019-01-30 18:47:44 -0700

    Added put route to update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    db('methods')
+    db('methods').orderBy('id')
     .then((rows) => {
       res.send(rows);
     })
@@ -29,6 +29,28 @@ app.post("/", (req, res, next) => {
     "tags": req.body.tag,
     "link": req.body.link
   })
+  .then((rows) => {
+    res.send(rows);
+    })
+  .catch((err) => {
+    next(err);
+  })
+})
+
+app.put("/update/:id", (req, res, next) => {
+  console.log(req.body.description)
+  // knex('wallabies').where('id', '=', req.params.id).update(req.body).returning('*')
+  db('methods')
+  .where('id', '=', req.params.id)
+  .update(req.body)
+  .returning('*')
+  // .insert({
+  //   "name": req.body.name,
+  //   "description": req.body.description,
+  //   "example": req.body.example,
+  //   "tags": req.body.tag,
+  //   "link": req.body.link
+  // })
   .then((rows) => {
     res.send(rows);
     })
